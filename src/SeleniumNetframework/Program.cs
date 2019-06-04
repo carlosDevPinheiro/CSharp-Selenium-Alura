8a3c5927f0e0135d913f0c398f4625c698dd9961
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2019-06-03 23:46:11 -0300

    Inicial

---
+++ src/SeleniumNetframework/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SeleniumNetframework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IWebDriver driver = new ChromeDriver(@"C:\dev\CSharp\CSharp-Selenium-Alura\src\SeleniumNetframework\Drivers");
+            driver.Navigate().GoToUrl("https://localhost:44323/");
+            IWebElement campoTexto = driver.FindElement(By.LinkText("Clientes"));
+            campoTexto.Click();
+            IWebElement campoAdicionar = driver.FindElement(By.LinkText("Adicionar novo cliente"));
+            campoAdicionar.Click();
+
+            IWebElement campoFirtName = driver.FindElement(By.Name("FirstName"));
+            IWebElement campoLastName = driver.FindElement(By.XPath("//input[@id='LastName']"));
+            campoFirtName.SendKeys("Carlos");
+            campoLastName.SendKeys("Pinheiro");
+            campoLastName.SendKeys(Keys.Tab);
+            //campoTexto.SendKeys("Caelum");
+            //campoTexto.Submit();
+            driver.Close();
+
+
+            // https://escoladedados.org/tutoriais/xpath-para-raspagem-de-dados-em-html/
+        }
+    }
+}
---
+++ src/TesteSelenium/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TesteSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\dev\CSharp\CSharp-Selenium-Alura\src\TesteSelenium\Drivers");
+            driver.Navigate().GoToUrl("https://localhost:44323/");
+            IWebElement campoTexto = driver.FindElement(By.LinkText("Clientes"));
+            campoTexto.Click();
+            IWebElement campoAdicionar = driver.FindElement(By.LinkText("Adicionar novo cliente"));
+            campoAdicionar.Click();
+
+            IWebElement campoFirtName = driver.FindElement(By.Name("FirstName"));
+            IWebElement campoLastName = driver.FindElement(By.XPath("//input[@id='LastName']"));
+            campoFirtName.SendKeys("Carlos");
+            campoLastName.SendKeys("Pinheiro");
+            campoLastName.SendKeys(Keys.Tab);
+            //campoTexto.SendKeys("Caelum");
+            //campoTexto.Submit();
+          //  driver.Close();
+        }
+    }
+}
